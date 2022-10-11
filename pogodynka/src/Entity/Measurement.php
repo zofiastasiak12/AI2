9b22f0380a9537367609103e8f05c2e892cfe8d0
zofiastasiak12 <zofia.stasiak15@gmail.com>
2022-10-11 18:43:54 +0200

    proba

---
+++ pogodynka/src/Entity/Measurement.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MeasurementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MeasurementRepository::class)]
+class Measurement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Location $location = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: '0')]
+    private ?string $celsius = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: '0')]
+    private ?string $wind = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: '0')]
+    private ?string $humidity = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: '0')]
+    private ?string $pressure = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLocation(): ?Location
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?Location $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getCelsius(): ?string
+    {
+        return $this->celsius;
+    }
+
+    public function setCelsius(string $celsius): self
+    {
+        $this->celsius = $celsius;
+
+        return $this;
+    }
+
+    public function getWind(): ?string
+    {
+        return $this->wind;
+    }
+
+    public function setWind(string $wind): self
+    {
+        $this->wind = $wind;
+
+        return $this;
+    }
+
+    public function getHumidity(): ?string
+    {
+        return $this->humidity;
+    }
+
+    public function setHumidity(string $humidity): self
+    {
+        $this->humidity = $humidity;
+
+        return $this;
+    }
+
+    public function getPressure(): ?string
+    {
+        return $this->pressure;
+    }
+
+    public function setPressure(string $pressure): self
+    {
+        $this->pressure = $pressure;
+
+        return $this;
+    }
+}
